1348a118d6bb2dbb277d8fc96792c50b53385728
Nathan Burns <nathanandreburns@gmail.com>
2020-08-05 17:19:33 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+katz_deli = []
+def line(array)
+  line_position = []
+  if array.empty?
+    puts "The line is currently empty."
+  else
+    counter = 1
+    array.each do |customer|
+      line_position << "#{counter}. #{customer}"
+      counter += 1
+    end
+    puts "The line is currently: #{line_position.join(" ")}"
+  end
+end
+
+def take_a_number(katz_deli, |customer|)
